6a749be8c9603d0756b6d345efc6e6680d5e2c7a
sean driard <seandriard3@gmail.com>
2021-09-27 17:25:41 +0200

    page accueil + bootstrap

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AccueilController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
-});
+});*/
+Route::get('accueil', 'App\Http\Controllers\AccueilController@Accueil')->name('Accueil');
