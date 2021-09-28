6f80978fdc65d805e6318ae3073f63f8ba237176
sean driard <seandriard3@gmail.com>
2021-09-28 11:28:14 +0200

    vue mounted

---
+++ resources/js/components/DragComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <DDR value={this.transform}>
+        <div style="width:100%;height:100%;background:red" />
+    </DDR>
+</template>
+
+<script>
+export default {
+    name: "DragComponent.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
+
+
