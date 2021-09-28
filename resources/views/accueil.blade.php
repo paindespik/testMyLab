b419c39e50163fe086bff39d9a3a3ddbea359e3b
Enzo Magnier <enzomagnier30@gmail.com>
2021-09-28 14:41:30 +0200

    question

--- resources/views/accueil.blade.php
+++ resources/views/accueil.blade.php
@@ -5,71 +5,97 @@
         TestMyLab
     </title>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
-   
-    <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/sticky-footer-navbar/">
-
-    <!-- Bootstrap core CSS -->
-    <link href="../../dist/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
-    <link href="sticky-footer-navbar.css" rel="stylesheet">
     <!-- CSS Files -->
     <link href="{{url('/')}}/assets/css/bootstrap.min.css" rel="stylesheet" />
 
 </head>
 <body>
 
-<header>
-      <!-- Fixed navbar -->
-      <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-        <a class="navbar-brand" href="#">Test My Lab</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarCollapse">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Link</a>
-            </li>
-          </ul>
-      
-        </div>
-      </nav>
-    </header>
-    <style>
-    #app{
-        margin-bottom: 10%;
-    }
-    #main{
-        height: 100%;
-        width: 100%;
-        left: 0;
-        top: 0;
-        overflow: hidden;
-        position: fixed;
-      }
-</style>
-    <main role="main" #id=main class="container">
-       
-      <section id=app>
+<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+<a class="navbar-brand" href="#">Navbar</a>
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+  <span class="navbar-toggler-icon"></span>
+</button>
 
-      </section>
-      <p>Back to <a href="../sticky-footer/">the default sticky footer</a> minus the navbar.</p>
-    </main>
-
-    <footer class="footer">
-      <div class="container">
-        <span class="text-muted">Place sticky footer content here.</span>
+<div class="collapse navbar-collapse" id="navbarsExampleDefault">
+  <ul class="navbar-nav mr-auto">
+    <li class="nav-item active">
+      <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Link</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link disabled" href="#">Disabled</a>
+    </li>
+    <li class="nav-item dropdown">
+      <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+      <div class="dropdown-menu" aria-labelledby="dropdown01">
+        <a class="dropdown-item" href="#">Action</a>
+        <a class="dropdown-item" href="#">Another action</a>
+        <a class="dropdown-item" href="#">Something else here</a>
       </div>
-    </footer>
+    </li>
+  </ul>
+
+</div>
+</nav>
+<main role="main">
+<style>
+.container{
+    text-align: center;
+}
+.display-3{
+    color: darkblue;
+}
+.containers{
+    text-align: center;
+    background-color: darkgray;
+}
+</style>
+<!-- Main jumbotron for a primary marketing message or call to action -->
+<div class="jumbotron">
+  <div class="container">
+    <h2 class="display-3">Question 1 : </h1>
+    <p>Comment est votre adressage IP  ?
+
+    <p>adresse Sectorisé (différent plage d'adresse séparé par des VLAN)</p>
+    
+   </p>
+   <p> adresse commune (même plage d'ip sur le réseau et 1 seule VLAN)</p>
+    <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>
+  </div>
+</div>
+<div class="jumbotrons">
+  <div class="containers">
+    <h1 class="display-3">
+    <p>Quel es la marque de votre matériel informatique ?</p></h1>
+    <select name="year_subject" id="year_subject">
+  <option value="theme1"><nav>Cisco</nav></option>
+  <option value="theme2"><nav>fortinet</nav></option>
+  <option value="theme3"><nav>alcatel lucent</nav></option>
+  <option value="theme4"><nav>netgear</nav></option>
+  <option value="theme5"><nav>hp</nav></option>
+  <option value="theme6"><nav>dell</nav></option>
+  <option value="theme7"><nav>ibm</nav></option>
+  <option value="theme8"><nav>autre</nav></option>
+
+  <input class="btn btn-primary btn-lg" type="submit" value="Envoyer le formulaire">
+
+
+</select> 
+
+  </div>
+</div>
+<div class="container">
+  <!-- Example row of columns -->
+  <hr>
+
+</div> <!-- /container -->
+
+</main>
 </body>
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
-    <script src="../../assets/js/vendor/popper.min.js"></script>
-    <script src="../../dist/js/bootstrap.min.js"></script>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 <script src="{{ url('/') }}/assets/js/jquery-3.6.0.min.js"></script>
 <script src="{{ url('/') }}/assets/js/bootstrap.min.js"></script>
 <script src="{{ asset('js/app.js') }}"></script>
